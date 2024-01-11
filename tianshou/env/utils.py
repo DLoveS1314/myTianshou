565cc452fec19ee586b87a332d25ef2051ee46dc
dls <1115330315@qq.com>
2024-01-11 23:50:34 +0800

    删除了以前的记录重新保存我的

---
+++ tianshou/env/utils.py
@@ -0,0 +1,26 @@
+from typing import Any
+
+import cloudpickle
+import gymnasium
+import numpy as np
+
+# from tianshou.env.pettingzoo_env import PettingZooEnv #暂且不用 
+
+# ENV_TYPE = gymnasium.Env | PettingZooEnv
+ENV_TYPE = gymnasium.Env  
+
+
+gym_new_venv_step_type = tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]
+
+
+class CloudpickleWrapper:
+    """A cloudpickle wrapper used in SubprocVectorEnv."""
+
+    def __init__(self, data: Any) -> None:
+        self.data = data
+
+    def __getstate__(self) -> str:
+        return cloudpickle.dumps(self.data)
+
+    def __setstate__(self, data: str) -> None:
+        self.data = cloudpickle.loads(data)
